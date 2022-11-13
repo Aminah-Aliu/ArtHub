a1b93105abb6c69d5cf7fe2e3c94ac334a526dc9
Alayna Nguyen <32422836+alaynamnguyen@users.noreply.github.com>
2022-11-13 02:24:12 -0500

    Tiny favicon update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// const collections = INSERT QUERY HERE
+const collections = [{"createdAt":"2022-11-12T23:10:35.152469Z","uuid":"35298ebf-0b80-43e2-a808-1d6ed8c45e51","name":"COLLECTION_NAME","description":"COLLECTION_DESCRIPTION","userId":577,"cid":""},
+{"createdAt":"2022-11-12T23:10:38.373357Z","uuid":"96b52078-7fba-413c-b984-fbf2d1115e0c","name":"COLLECTION_NAME","description":"v","userId":577,"cid":"","contributors":["test"]},
+{"createdAt":"2022-11-12T23:10:38.484493Z","uuid":"0ec07621-c8f1-42d1-b19a-935a09f34f2a","name":"COLLECTION_NAME","description":"vi","userId":577,"cid":""}]
+
 const allVids = document.querySelector('.userVideos')
 fetch('../html/allvideos.html')
 .then(res=>res.text())
@@ -21,11 +26,28 @@ newCard = `<div class="col-sm-4">
 </div>
 </div>`
 var limit = 4;
-for (var i=0; i < limit; i++) {
+// var add = ;
+const para = document.createElement("p");
+const node = document.createTextNode("This is a paragraph.");
+
+para.appendChild(node);
+console.log(document.getElementsByClassName("dynCards")[0]);
+document.getElementById("dynCardsStart").appendChild(para);
+
+// for (var i=0; i < limit; i++) {
   
-    $('.dynCards').append(newCard);
+//     add.append(newCard);
     
-    // check index is counting correctly
-    console.log(i);
+//     // check index is counting correctly
+//     console.log(i);
     
-  }
+//   }
+
+// for (var i=0; i < limit; i++) {
+  
+//     $('.dynCards').append(newCard);
+    
+//     // check index is counting correctly
+//     console.log(i);
+    
+//   }
